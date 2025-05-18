8c000320a9de25b2304e54319154b267cb06e916
vrushalibavare <vrushali.bavare@gmail.com>
2025-05-18 22:25:59 +0800

    modified to use variables for instance type and key-name

--- tf-ec2-example/main.tf
+++ tf-ec2-example/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "public" {
   ami = data.aws_ami.latest_amazon_linux.id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   subnet_id = data.aws_subnet.selected.id
   associate_public_ip_address = true
-  key_name = "vrush"
+  key_name = var.key_name
   vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
   tags = {
     Name = "${var.name}-ec2"
--- tf-ec2-example/variables.tf
+++ tf-ec2-example/variables.tf
@@ -2,3 +2,14 @@ variable "name" {
   type = string
   description = "name of the app"
 }
+
+variable instance_type {
+  type = string
+  description = "EC2 instance type"
+  default = "t2.micro"
+}
+ variable "key_name" {
+  type = string
+  description = "SSH key name"
+   
+ }
