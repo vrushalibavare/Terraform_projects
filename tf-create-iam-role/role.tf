2a01a2cf7fdf97b8f24edbf8f700611f8fca03d2
vrushalibavare <vrushali.bavare@gmail.com>
2025-05-20 13:16:15 +0800

    renamed tf-iam-roles to tf-create-iam-roles

---
+++ tf-create-iam-role/role.tf
@@ -0,0 +1,82 @@
+locals {
+  name_prefix = "var.name_prefix"
+}
+
+resource "aws_iam_role" "role_example" {
+  name = "${local.name_prefix}-tf-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Sid = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+
+#Option 1 - Using policy document defined in data.tf
+resource "aws_iam_policy" "policy_document_example" {
+  name = "${local.name_prefix}-role-policy"
+  description = "Example policy"
+  policy = data.aws_iam_policy_document.policy_document_example.json
+}
+  
+resource "aws_iam_role_policy_attachment" "attach_example" {
+  role       = aws_iam_role.role_example.name
+  policy_arn = aws_iam_policy.policy_document_example.arn
+}
+
+resource "aws_iam_instance_profile" "instance_profile_example" {
+  name = "${local.name_prefix}-instance-profile"
+  role = aws_iam_role.role_example.name
+}
+
+# Option 2 - Using inline policy
+
+# resource "aws_iam_role_policy" "inline-policy-example" {
+#   name = "${local.name_prefix}-inline-policy"
+#   role = aws_iam_role.role-example.name
+#   policy = jsondecode({
+#     version = "2012-10-17"
+#     statement = [
+#       {
+#         effect = "Allow"
+#         action = ["s3:ListBucket"]
+#         resource = ["*"]
+#       },
+#       {
+#         effect = "Allow"
+#         action = ["ec2:DescribeInstances"]
+#         resource = ["*"]
+#       }
+#     ]
+#   })
+# }
+
+# Option 3 - Using the policy inline heredoc
+# resource "aws_iam_role_policy" "inline-heredoc-policy-example" {
+#   name = "${local.name_prefix}-inline-heredoc-policy"
+#   role = aws_iam_role.role-example.name
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Action": "s3:ListBucket",
+#       "Resource": "*"
+#     },
+#     {
+#       "Effect": "Allow",
+#       "Action": "ec2:DescribeInstances",
+#       "Resource": "*"
+#     }
+#   ]
+# }
+# EOF
