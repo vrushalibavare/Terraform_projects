e20c7c5d117ec3eb4be5acec47a28ec2eeb0b5d2
vrushalibavare <vrushali.bavare@gmail.com>
2025-05-24 12:58:23 +0800

    removed acl public policy

--- tf-s3-static-web-hosting/s3.tf
+++ tf-s3-static-web-hosting/s3.tf
@@ -6,9 +6,7 @@ resource "aws_s3_bucket" "static_bucket" {
 
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.static_bucket.id
-  block_public_acls       = false
   block_public_policy     = false
-  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
