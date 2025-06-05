29a710034a3b5a4492f9a9d810ac702de8a818ca
vrushalibavare <vrushali.bavare@gmail.com>
2025-06-05 12:40:28 +0800

    updated vpc to include database subnets

--- tf-vpc-module-example/main.tf
+++ tf-vpc-module-example/main.tf
@@ -7,11 +7,14 @@ module "vpc" {
   azs = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1], data.aws_availability_zones.available.names[2]]
   private_subnets = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
   public_subnets = ["10.0.101.0/24","10.0.102.0/24","10.0.103.0/24"]
+  database_subnets = ["10.0.201.0/24","10.0.202.0/24","10.0.203.0/24"]
+  create_database_subnet_group = true
   enable_nat_gateway = true
   single_nat_gateway = true
   enable_dns_hostnames = true
   tags = {
     terraform = "true"
+    name     = "vrushali-tf-vpc"
 }
 }
 
