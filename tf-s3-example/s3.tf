7c6abb316cf402b4ebff6cabe6d6b46efc95a43e
vrushalibavare <vrushali.bavare@gmail.com>
2025-07-07 18:05:22 +0800

    modified bucket name

--- tf-s3-example/s3.tf
+++ tf-s3-example/s3.tf
@@ -1,4 +1,4 @@
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "vrush-tf-sample-bucket"  #Use a globally unique name
+  bucket = "vrush-tf-state-bucket"  #Use a globally unique name
   force_destroy = true
 }
